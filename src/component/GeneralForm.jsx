ef13bb749b387540c26e08c2521364f37fb82df6
Sarita <sarita@webexcellis.com>
2022-06-21 12:20:17 +0530

    password validation

--- src/component/GeneralForm.jsx
+++ src/component/GeneralForm.jsx
@@ -1,13 +1,65 @@
+import { useState } from "react";
+
 const GeneralInput = () => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [error, setError] = useState("");
+
+  const changeHandler = (e) => {
+    if (e.target.value && !Number.isFinite(parseInt(e.target.value))) {
+      setError("Please enter numeric digit having sum equal to 10");
+    } else {
+      setEnteredValue(e.target.value);
+    }
+  };
+
+  const passwordValidation = () => {
+    if (enteredValue.trim() === "") {
+      setError("This field is required !!!");
+      return;
+    } else {
+      const res =
+        enteredValue && enteredValue.split("").reduce((acc, cv) => +acc + +cv);
+      if (res !== 10) {
+        setError("Added value is not equal to 10");
+        return;
+      } else {
+        setError("");
+      }
+    }
+  };
+
+  const blurHandler = (e) => {
+    passwordValidation();
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    passwordValidation();
+  };
+
+  const isValid = !error;
+
+  const passwordValidationClasses = !isValid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-control">
         <label htmlFor="email">Your Email</label>
         <input type="email" id="email" />
       </div>
-      <div className="form-control">
+      <div className={passwordValidationClasses}>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          onChange={changeHandler}
+          onBlur={blurHandler}
+          value={enteredValue}
+        />
+        {!isValid && <p className="error-text">{error}</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
