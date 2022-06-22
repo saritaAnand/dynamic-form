f5819516f758dec79cef493972753e351234a714
Sarita <sarita@webexcellis.com>
2022-06-22 09:28:45 +0530

    multiselect-basic

--- src/component/Select.jsx
+++ src/component/Select.jsx
@@ -1,29 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "../common/Search";
+import MultiSelect from "../common/MultiSelect";
 
 const Select = () => {
+  const [isSearchable, setIsSearchable] = useState(false);
   return (
     <>
-      <label for="color-choice">Choose Color:</label>
-      <input
-        list="redColors"
-        id="color-choice"
-        name="color-choice"
-        // className="form-control"
-        type="color"
-      />
-
-      <select multiple>
-        <datalist id="redColors">
-          <option value="#800000" />
-          <option value="#8000ff" />
-          <option value="#8B0000" />
-          <option value="#A52A2A" />
-          <option value="#00ff00" />
-          <option value="#DC143C" />
-          <option value=" #ff0000" />
-          <option value="#00cc00" />
-        </datalist>
-      </select>
+      {/* <Search /> */}
+      <MultiSelect />
     </>
   );
 };
